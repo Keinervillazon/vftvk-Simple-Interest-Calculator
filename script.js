90a86e95523d6a6bf7b938c13f419474ac1c91e7
Keinervillazon <keiner.villazon@outlook.com>
2021-11-05 13:06:44 -0500

    Changes to final project

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 function compute()
 {
-    var c = document.getElementById('amount').value;
-    var i = document.getElementById('rate').value;
-    var t = document.getElementById('years').value;
-    var fecha= new Date();
-    var year = parseInt(fecha.getFullYear())+parseInt(t);
-    var receive = c*(i/100)*t;
+    var principal  = document.getElementById('principal').value;
+    var rate = document.getElementById('rate').value;
+    var years = document.getElementById('years').value;
 
-    document.getElementById('amountval').innerHTML=' '+ c +',';
-    document.getElementById('rateval').innerHTML=' '+ i +'%.';
-    document.getElementById('receive').innerHTML=' '+ receive.toFixed(2) +',';
-    document.getElementById('yearval').innerHTML=' '+ year +'.';
+    var interest  = principal * years * ( rate/100 );
+    var year = new Date().getFullYear()+parseInt(years);
     checkdata();
+
+    document.getElementById('result').innerHTML='If you deposit \<mark>'+ principal  +',\</mark>\<br\>at an interest rate of \<mark>'+ rate +'%.\</mark>\<br\>You will receive an amount of \<mark>'+ interest.toFixed(2) +',\</mark>\<br\>in the year \<mark>'+year+'\</mark>\<br\>';
 }
+
 function myOnload()
 {
-    var i = document.getElementById('rate').value;
-    document.getElementById('ratevalue').innerHTML=' '+ i +'%';
     years();
     hiddenResults();
 }
 function years()
 {
-    select = document.getElementById("years");
-for(i = 1; i <= 25; i++){
-    option = document.createElement("option");
-    option.value = i;
-    option.text = i;
-    select.appendChild(option);
-}
+    select = document.getElementById('years');
+    for(i = 1; i <= 10; i++){
+        option = document.createElement('option');
+        option.value = i;
+        option.text = i;
+        select.appendChild(option);
+    }
 }
 
-function rateChange()
+function updateRate()
 {
-    var r = document.getElementById('rate').value;
-    document.getElementById('ratevalue').innerHTML=' '+r+'%';
+    var rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerHTML=' '+rateval+'%';
 }
 function showResults() {
-    var v = document.getElementById("result");
-        v.style.display = "block";
+    //Show calculation results area
+    var v = document.getElementById('result');
+    v.style.display = 'block';
 }
 function hiddenResults() {
-    var v = document.getElementById("result");
-        v.style.display = "none";
+    //Hidde calculation results area
+    var v = document.getElementById('result');
+    v.style.display = 'none';
 }
 function checkdata(){
     //create references to the input elements we wish to validate
-    var amount = document.getElementById("amount");
-    var rate = document.getElementById("rate");
+    var amount = document.getElementById('principal');
 
     //Check if amount field is empty
-    if(amount.value == ""){
-          alert("Please enter a valid amount");
+    if(amount.value == ''){
+          alert('Please enter a valid amount');
+          amount.value='';
           amount.focus();
+          hiddenResults();
           return false;
-    }else if(parseInt(amount.value) == 0){
-          alert("Please enter a nonzero value");
+    }else if(parseInt(amount.value) <= 0 ){
+          alert('Please enter a positive number or greater than zero');
+          amount.value='';
           amount.focus();
+          hiddenResults();
           return false;
-
     }
     showResults();
     return true;
